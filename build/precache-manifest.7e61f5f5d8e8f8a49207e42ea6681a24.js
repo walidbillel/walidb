9cbb91632b0621f85b8f7bebd6b2d0b157beda5b
willou <35076190+walidbillel@users.noreply.github.com>
2020-10-31 19:52:42 -0400

    done

---
+++ build/precache-manifest.7e61f5f5d8e8f8a49207e42ea6681a24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06db405c267cdd7b0657f36f3cb54ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ec1f1e8fc1c698415f",
+    "url": "/static/css/main.f4a74042.chunk.css"
+  },
+  {
+    "revision": "7f9e98ca54d67de88bd1",
+    "url": "/static/js/2.5f7d914f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f7d914f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ec1f1e8fc1c698415f",
+    "url": "/static/js/main.a53940bb.chunk.js"
+  },
+  {
+    "revision": "507c2108c42defe9493d",
+    "url": "/static/js/runtime-main.9a5aa918.js"
+  },
+  {
+    "revision": "36913fac2757a0220a7a7cb927aa6d92",
+    "url": "/static/media/logo-for-black.36913fac.svg"
+  },
+  {
+    "revision": "42ecaeda01de0b532d1e09d0d8e7e83b",
+    "url": "/static/media/logo-for-white.42ecaeda.svg"
+  }
+]);
